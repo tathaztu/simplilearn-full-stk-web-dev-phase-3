8016418f2d62373dbb7727b2b410410673ed28bd
Medha Bambroo <medha.84@gmail.com>
2022-07-31 21:40:17 -0400

    07/31/2022

---
+++ 09-hello-world-spring-boot/src/main/java/com/simplilearn/workshop/model/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.simplilearn.workshop.model;
+
+public class HelloWorld {
+
+	private String message;
+
+	public HelloWorld(String message) {
+		this.message = message;
+	}
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
